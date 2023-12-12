133a28ac86f5ed25adc94d66ab5ca9337bb49f02
williamtanuwijaya <will.tw0807@gmail.com>
2023-12-12 10:20:22 +0700

    konfigurasi inertiajs

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -35,6 +36,17 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+    }
+
+    public function map()
+    {
+        $this->mapApiRoutes();
+
+        $this->mapWebRoutes();
+
+        Inertia::macro('share', function ($key, $value) {
+            Inertia::share($key, $value);
         });
     }
 
